2a1f4369092168ab52a68aa42fcb9767bdfb5c86
nevesade <adenasiocarvalho19@gmail.com>
2021-03-25 15:22:54 +0000

    Creates Project App Bank Transfert

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppBankTransfert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
